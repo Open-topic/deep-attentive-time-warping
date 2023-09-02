a58155c3aaf621007ba84a5c907dc54ed2bcc661
Wintoplay <66968226+Wintoplay@users.noreply.github.com>
2023-09-02 21:42:30 +0700

    fix

--- code/multiple-gpu-main-pre-training.py
+++ code/multiple-gpu-main-pre-training.py
@@ -19,59 +19,14 @@ log = logging.getLogger(__name__)
 
 import omegaconf
 import os
-
-@ hydra.main(config_path='conf', config_name='pre_training')
 class unet(L.LightningModule):
     def __init__(self):
         super().__init__()
-        fix_seed(cfg.seed)
-        cwd = hydra.utils.get_original_cwd()+'/'
-
-        # load data (split train data & standardizarion)
-        dataset = get_UCRdataset(cwd, cfg)
-
-        # make result folder
-        result_path = '%s%sresult/' % (cwd, cfg.result_path)
-        make_folder(path=result_path)
-        result_path += '%s_%s/' % (str(cfg.dataset.ID).zfill(3),
-                                dataset.dataset_name)
-        make_folder(path=result_path)
-        result_path += 'pre_training/'
-        make_folder(path=result_path)
-        result_path += '%s' % dataset.dataset_name
-
-        # log saved at result folder
-        file_handler = logging.FileHandler(
-            '%s.log' % (result_path), 'a')
-        log.addHandler(file_handler)
-
-        log.info('\n=============================================================')
-        log.debug(OmegaConf.to_yaml(cfg), cfg)
-        log.info('dataset ID: %d, dataset name: %s' %
-                (cfg.dataset.ID, dataset.dataset_name))
-
-        # If the number of training + validation data is less than the threshold, do not execute.
-        log.info('Number of training + validation data: %d' %
-                (dataset.N_train_data+dataset.N_val_data))
-        if dataset.N_train_data+dataset.N_val_data < cfg.dataset.used_dataset_threshold.num_train_data:
-            log.info('The number of training data is less than %d ...' %
-                    cfg.dataset.used_dataset_threshold.num_train_data)
-            log.info('It is not executed.')
-            exit()
-
-        # If the length of data is more than the threshold, do not execute.
-        log.info('Length of data: %d' % dataset.length)
-        if dataset.length > cfg.dataset.used_dataset_threshold.length_data:
-            log.info('The number of training data is more than %d ...' %
-                    cfg.dataset.used_dataset_threshold.length_data)
-            log.info('It is not executed.')
-            exit()
-
         # define model & optimizer & loss function
-        model = ProposedModel(input_ch=dataset.channel).to(cfg.device)
-        model_summary = torchinfo.summary(
-            model, (dataset.train_data[:1].shape, dataset.train_data[:1].shape), device=cfg.device, verbose=0)
-        log.debug(model_summary)
+        self.model = ProposedModel(input_ch=dataset.channel).to(cfg.device)
+        self.model_summary = torchinfo.summary(
+            self.model, (dataset.train_data[:1].shape, dataset.train_data[:1].shape), device=cfg.device, verbose=0)
+        log.debug(self.model_summary)
         self.loss_function = nn.MSELoss()
         self.train_losses =[]
         self.val_losses=[]
@@ -82,15 +37,15 @@ class unet(L.LightningModule):
         loss = self.loss_function(
             F.softmax(y, dim=2), F.softmax(path, dim=2))
         self.log("train_loss", loss)
-        training_losses.append(loss.item())
+        self.training_losses.append(loss.item())
         return loss
     
     def validation_step(self, batch, batch_idx):
         data1, data2, path, _ =batch
-        y = model(data1, data2)
+        y = self.model(data1, data2)
         loss = loss_function(
             F.softmax(y, dim=2), F.softmax(path, dim=2))
-        val_losses.append(loss.item())
+        self.val_losses.append(loss.item())
         self.log("validation_loss", loss)
 
     def configure_optimizers(self):
@@ -101,36 +56,78 @@ class unet(L.LightningModule):
 #cfg = c[0]
 #print(cfg)
 
-cwd = os.getcwd()+'/'
-path = cwd+"code/"+"conf/"+'pre_training.yaml'
-cfg = omegaconf.OmegaConf.load(path)
-dataset = get_UCRdataset(cwd, cfg)
 
-# make data loader
-# train
-train_dataset = DatasetPreTraining(dataset, 'train', cfg)
-if cfg.train_loader_balance:
-    train_batch_sampler = BalancedBatchSampler(
-        dataset, 'train', cfg.positive_ration, cfg.negative_ration, cfg)
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_sampler=train_batch_sampler, num_workers=cfg.num_workers)
-else:
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=cfg.batch_size, num_workers=cfg.num_workers, shuffle=True)
-log.info('Length of train_loader: %d' % len(train_loader))
+@ hydra.main(config_path='conf', config_name='pre_training')
+def main(cfg: DictConfig) -> None:
+    cwd = hydra.utils.get_original_cwd()+'/'
+    fix_seed(cfg.seed)
+    cwd = hydra.utils.get_original_cwd()+'/'
 
-# val
-val_dataset = DatasetPreTraining(dataset, 'val', cfg)
-if cfg.val_loader_balance:
-    val_batch_sampler = BalancedBatchSampler(
-        dataset, 'val', cfg.positive_ration, cfg.negative_ration, cfg)
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_sampler=val_batch_sampler, num_workers=cfg.num_workers)
-else:
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=cfg.batch_size, num_workers=cfg.num_workers, shuffle=True)
-log.info('Length of val_loader: %d' % len(val_loader))
+    # load data (split train data & standardizarion)
+    dataset = get_UCRdataset(cwd, cfg)
 
-# Lightning will automatically use all available GPUs!
-trainer = L.Trainer()
-trainer.fit(unet(), train_dataloader=train_loader,val_dataloader = val_loader)
+    # make result folder
+    result_path = '%s%sresult/' % (cwd, cfg.result_path)
+    make_folder(path=result_path)
+    result_path += '%s_%s/' % (str(cfg.dataset.ID).zfill(3),
+                               dataset.dataset_name)
+    make_folder(path=result_path)
+    result_path += 'pre_training/'
+    make_folder(path=result_path)
+    result_path += '%s' % dataset.dataset_name
+
+    # log saved at result folder
+    file_handler = logging.FileHandler(
+        '%s.log' % (result_path), 'a')
+    log.addHandler(file_handler)
+
+    log.info('\n=============================================================')
+    log.debug(OmegaConf.to_yaml(cfg), cfg)
+    log.info('dataset ID: %d, dataset name: %s' %
+             (cfg.dataset.ID, dataset.dataset_name))
+
+    # If the number of training + validation data is less than the threshold, do not execute.
+    log.info('Number of training + validation data: %d' %
+             (dataset.N_train_data+dataset.N_val_data))
+    if dataset.N_train_data+dataset.N_val_data < cfg.dataset.used_dataset_threshold.num_train_data:
+        log.info('The number of training data is less than %d ...' %
+                 cfg.dataset.used_dataset_threshold.num_train_data)
+        log.info('It is not executed.')
+        exit()
+
+    # If the length of data is more than the threshold, do not execute.
+    log.info('Length of data: %d' % dataset.length)
+    if dataset.length > cfg.dataset.used_dataset_threshold.length_data:
+        log.info('The number of training data is more than %d ...' %
+                 cfg.dataset.used_dataset_threshold.length_data)
+        log.info('It is not executed.')
+        exit()
+
+    # make data loader
+    # train
+    train_dataset = DatasetPreTraining(dataset, 'train', cfg)
+    if cfg.train_loader_balance:
+        train_batch_sampler = BalancedBatchSampler(
+            dataset, 'train', cfg.positive_ration, cfg.negative_ration, cfg)
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_sampler=train_batch_sampler, num_workers=cfg.num_workers)
+    else:
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=cfg.batch_size, num_workers=cfg.num_workers, shuffle=True)
+    log.info('Length of train_loader: %d' % len(train_loader))
+
+    # val
+    val_dataset = DatasetPreTraining(dataset, 'val', cfg)
+    if cfg.val_loader_balance:
+        val_batch_sampler = BalancedBatchSampler(
+            dataset, 'val', cfg.positive_ration, cfg.negative_ration, cfg)
+        val_loader = torch.utils.data.DataLoader(
+            val_dataset, batch_sampler=val_batch_sampler, num_workers=cfg.num_workers)
+    else:
+        val_loader = torch.utils.data.DataLoader(
+            val_dataset, batch_size=cfg.batch_size, num_workers=cfg.num_workers, shuffle=True)
+    log.info('Length of val_loader: %d' % len(val_loader))
+
+    # Lightning will automatically use all available GPUs!
+    trainer = L.Trainer()
+    trainer.fit(unet(), train_dataloader=train_loader,val_dataloader = val_loader)
