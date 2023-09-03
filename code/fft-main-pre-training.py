2be2d98856bdf9c8d2d55ecda1f16a4f508d6e5d
Wintoplay <66968226+Wintoplay@users.noreply.github.com>
2023-09-03 16:12:05 +0700

    fix

--- code/fft-main-pre-training.py
+++ code/fft-main-pre-training.py
@@ -66,7 +66,7 @@ def main(cfg: DictConfig) -> None:
     model = fft_ProposedModel(input_ch=dataset.channel).to(cfg.device)
     #model_summary = torchinfo.summary(
     #    model, (dataset.train_data[:1].shape, dataset.train_data[:1].shape), device=cfg.device, verbose=0)
-    log.debug(model_summary)
+    #log.debug(model_summary)
     optimizer = optim.AdamW(model.parameters(), lr=cfg.lr, betas=(0.5, 0.999))
     loss_function = nn.MSELoss()
 
