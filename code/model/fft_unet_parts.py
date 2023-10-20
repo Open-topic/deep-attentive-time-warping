23b2ee9cde9d6583438c69ce4627d3f04d52b2ae
Wintoplay <winroom@gmail.com>
2023-10-20 16:27:19 +0700

    distangle fft from upsampling layers

--- code/model/fft_unet_parts.py
+++ code/model/fft_unet_parts.py
@@ -36,6 +36,24 @@ class FFT_DoubleConv(nn.Module):
 
         return x
 
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.GELU(),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU()
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
 
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
@@ -61,11 +79,11 @@ class Up(nn.Module):
         if bilinear:
             self.up = nn.Upsample(
                 scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = FFT_DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
             self.up = nn.ConvTranspose2d(
                 in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = FFT_DoubleConv(in_channels, out_channels)
+            self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
